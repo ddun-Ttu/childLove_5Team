eba33e02cb2fff8517cbb30d4539d83e0e984c53
bongsub kim <bong989971@gmail.com>
2023-05-27 17:12:54 +0900

    feat XMLParser

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import XMLParser from "react-xml-parser";
 
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const [arr, setArr] = useState();
+
+  console.log(arr);
+
+  useEffect(() => {
+    fetch(
+      "https://apis.data.go.kr/B552657/HsptlAsembySearchService/getHsptlMdcncFullDown?serviceKey=aQFwyyURxZPboOkpSx1uUEC9mvyECY1ClICrCdzJ9lNT9JZC0oGtU%2BKwiY7dSTrZm3wodyTWqkdltlLRwKFafQ%3D%3D&pageNo=1&numOfRows=10"
+    )
+      .then((res) => res.text())
+      .then((data) => {
+        const xml = new XMLParser().parseFromString(data);
+        setArr(xml.children[1].children[0].children);
+      })
+      .catch(() => console.log("err"));
+  }, []);
+
+  return <div></div>;
 }
 
 export default App;
