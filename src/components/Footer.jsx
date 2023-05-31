d7999748cfca791c967ac5aa5506d73debf6d019
정민규 <wjalsrb0923@gmail.com>
2023-05-31 14:03:18 +0900

    Design : Footer 가운데 정렬

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,24 +4,25 @@ import styled from "styled-components";
 export const Footer = () => {
   return (
     <>
-      <FooterText>
+      <FooterBox>
         <FooterTitle>[웹사이트 명]</FooterTitle>
         <FooterLink href="https://kdt-gitlab.elice.io/sw_track/class_04/web_2_project/team05/front-end">
           https://kdt-gitlab.elice.io/sw_track/class_04/web_2_project/team05/front-end
         </FooterLink>
         <p>2023-05-26 ~ 2023-06-16</p>
-      </FooterText>
+      </FooterBox>
     </>
   );
 };
 
-const FooterText = styled.div`
+const FooterBox = styled.div`
   font-size: 12px;
   text-align: center;
   color: #606060;
   width: 834px;
   height: 126px;
   background-color: #f3f3f3;
+  margin: 0px auto 0px auto;
 `;
 
 const FooterTitle = styled.p`
