8d65fa070b27df9adf8bbea791afc34c2ad3f578
junmiLee <junmi950918@gmail.com>
2023-06-03 05:24:48 +0900

    feat : 더미 데이터 파일 추가 및 캐러셀 슬라이드 기능 추가

---
+++ src/pages/data.js
@@ -0,0 +1,38 @@
+export const dataHome = [
+  {
+    id: 1,
+    hospitalName: "나라사랑 병원",
+    address: "경기도 김포시 마산동 328-32 밍뚜센터 5층",
+    linkImg: "https://picsum.photos/283/283/?blur/random=1​",
+  },
+  {
+    id: 2,
+    hospitalName: "밍뚜병원",
+    address: "서울특별시 강남구 역삼동 123-45",
+    linkImg: "https://picsum.photos/283/283/?blur​/random=2",
+  },
+  {
+    id: 3,
+    hospitalName: "나라사랑 병원",
+    address: "경기도 김포시 마산동 328-32 밍뚜센터 1층",
+    linkImg: "https://picsum.photos/283/283/?blur​/random=3",
+  },
+  {
+    id: 4,
+    hospitalName: "미미병원",
+    address: "서울특별시 강남구 역삼동 123-45",
+    linkImg: "https://picsum.photos/283/283/?blur​/random=4",
+  },
+  {
+    id: 5,
+    hospitalName: "채원 병원",
+    address: "경기도 김포시 마산동 328-32 밍뚜센터 2층",
+    linkImg: "https://picsum.photos/283/283/?blur​/random=5",
+  },
+  {
+    id: 6,
+    hospitalName: " 유진 병원",
+    address: "서울특별시 강남구 역삼동 123-45",
+    linkImg: "https://picsum.photos/283/283/?blur​/random=6",
+  },
+];
