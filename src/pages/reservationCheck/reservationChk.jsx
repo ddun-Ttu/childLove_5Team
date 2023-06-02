2e88a38f77e9b6b283d8ba18873b6f0cf48b4b42
2UJIN <83401978+2UJ1N@users.noreply.github.com>
2023-06-02 21:53:51 +0900

    feat : 예약현황 페이지 생성 후 하나에 조합

---
+++ src/pages/reservationCheck/reservationChk.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { ReservationMemo } from "./memoModal";
+import { MyCalendar } from "./myCalendar";
+
+const ReservationChk = () => {
+  return (
+    <>
+      <MyCalendar />
+      <ReservationMemo />
+    </>
+  );
+};
+
+export default ReservationChk;
