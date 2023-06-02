0f0b683bdc665d89302c6f9b20df8443d04b31a8
2UJIN <83401978+2UJ1N@users.noreply.github.com>
2023-06-02 18:19:25 +0900

    feat : 날짜 + 캘린더 카드박스 내부에 넣음

---
+++ src/pages/reservationCheck/MyCalendar.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import moment from "moment";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import CardBox from "../../components/CardBox";
+import styled from "styled-components";
+
+export const MyCalendar = () => {
+  const [value, onChange] = useState(new Date());
+
+  return (
+    <>
+      <CardBox linkTo={"#"}>
+        <ShowDate>
+          <h1>{moment(value).format("YY. MM. DD")}</h1>
+        </ShowDate>
+      </CardBox>
+      <CardBox linkTo={"#"}>
+        <ShowCalendar>
+          <Calendar onChange={onChange} value={value} />
+        </ShowCalendar>
+      </CardBox>
+    </>
+  );
+};
+
+const ShowDate = styled.div`
+  width: 100%;
+  border-bottom: 1px solid #b2b2b2;
+  display: flex;
+  text-align: center;
+  justify-content: center;
+
+  & h1 {
+    font-size: 30px;
+    color: #00ad5c;
+    font-weight: bold;
+    text-decoration-line: none;
+  }
+`;
+
+const ShowCalendar = styled.div`
+  width: 100%;
+  border-bottom: 1px solid #b2b2b2;
+  display: flex;
+  text-align: center;
+  justify-content: center;
+`;
