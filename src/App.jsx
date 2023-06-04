604b50e27a35af338a07e87413647e43152be95c
junmiLee <junmi950918@gmail.com>
2023-06-04 10:55:14 +0900

    rename : 폴더 구성 변경

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,10 @@ import XMLParser from "react-xml-parser";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 // 공통 컴포넌트 연결해서 테스트함
-import { Button } from "./components/button";
 import { NavigationBar } from "./components/NavigationBar";
 import { Container } from "./components/Container";
 import { Footer } from "./components/Footer";
-import { CardBox } from "./components/CardBox";
 import { Home } from "./pages/Home";
-import { Login } from "./pages/Login";
-import { SignUp } from "./pages/SignUp";
 
 // 상수로 뽑아둔 color, fontSize 연결 링크
 import colors from "./constants/colors";
@@ -45,8 +41,7 @@ function App() {
     <>
       <Container>
         <Router>
-          <Login />
-          <SignUp />
+          <Home />
           <Footer />
           <Navigator />
         </Router>
