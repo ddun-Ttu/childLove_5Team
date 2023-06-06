dd8efa2ceb829fcf13b4bc3465442c8c9aaf05ea
정민규 <wjalsrb0923@gmail.com>
2023-06-06 19:27:31 +0900

    Fix: 삭제 파일 복구

---
+++ src/components/Button.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+export const Button = () => {
+  return (
+    <>
+      <Button2></Button2>
+    </>
+  );
+};
+
+export const Button2 = styled.button`
+  width: 120px;
+  color: black;
+`;
