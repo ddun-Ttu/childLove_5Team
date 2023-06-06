ac3e85ea0b0d1aefb8ee1876e839cc11c2fe336d
2UJIN <83401978+2UJ1N@users.noreply.github.com>
2023-06-06 15:58:02 +0900

    feat : 페이지 공통 컴포넌트 추가

--- src/pages/reservationCheck/reservationChk.jsx
+++ src/pages/reservationCheck/reservationChk.jsx
@@ -2,11 +2,29 @@ import React from "react";
 import { ReservationMemo } from "./memoModal";
 import { MyCalendar } from "./MyCalendar";
 
+// 공통 컴포넌트 연결해서 테스트함
+import {
+  Button,
+  CardBox,
+  Header,
+  NavigationBar,
+  Container,
+  Footer,
+  SearchBar,
+} from "../../components/index";
+
+// 상수로 뽑아둔 color, fontSize 연결 링크
+import colors from "../../constants/colors";
+import fontSize from "../../constants/fontSize";
+
 const ReservationChk = () => {
   return (
     <>
+      <Header label={"예약 현황"} />
       <MyCalendar />
       <ReservationMemo label={"아이사랑 소아과"} />
+      <Footer />
+      <NavigationBar />
     </>
   );
 };
