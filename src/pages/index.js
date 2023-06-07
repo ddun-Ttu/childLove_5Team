6c867b48b39ad15ea13db303ee23e340f161275e
chaeyeon <chaeyni.dev@gmail.com>
2023-06-07 23:51:05 +0900

    Feat: 모달 컴포넌트 추가 & 검색창 컴포넌트 수정

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,6 @@ import { Post } from "./registerForm/Post";
 // import { MyCalendar } from "./reservationCheck/MyCalendar";
 import HospitalCard from "./search/HospitalCard";
 import { SignUp } from "./signUp/SignUp";
-import  Detail  from "./detail/Detailpage";
+import Detail from "./detail/Detailpage";
 
-export { AdminHome, Home, MyPage, Login, Post, HospitalCard, SignUp, Detail, };
+export { AdminHome, Home, MyPage, Login, Post, HospitalCard, SignUp, Detail };
