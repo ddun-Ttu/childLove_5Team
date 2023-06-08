e4116bf0a360a5a21f1845a210c718545bb8dc8d
정민규 <wjalsrb0923@gmail.com>
2023-06-09 01:55:55 +0900

    Feat : 카카오 api 사용하여 도로명 주소에서 위도,경도 추출

--- src/pages/registerForm/Post.jsx
+++ src/pages/registerForm/Post.jsx
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import colors from "../../constants/colors";
 import { InputBox, InputContent, InputName } from "./RegisterForm";
 import { Button } from "../../components/Button";
+import axios from "axios";
 
 export const Post = ({ getAddrData }) => {
   const [isOpen, setIsOpen] = useState(false); //모달 상태
@@ -22,7 +23,7 @@ export const Post = ({ getAddrData }) => {
   };
 
   // 주소 검색 다음 api
-  const handleComplete = (data) => {
+  const handleComplete = async (data) => {
     let fullAddress = data.address;
     let extraAddress = "";
 
@@ -37,14 +38,39 @@ export const Post = ({ getAddrData }) => {
       fullAddress += `${extraAddress !== "" ? ` ${extraAddress}` : ""}`;
     }
 
+    const convertAddressToCoordinates = async () => {
+      const url = `https://dapi.kakao.com/v2/local/search/address.json?query=${fullAddress}`;
+      const headers = {
+        Authorization: `KakaoAK ${process.env.REACT_APP_KAKAO_API_KEY}`,
+      };
+
+      try {
+        const response = await axios.get(url, { headers });
+
+        if (response.status === 200) {
+          const lng = parseFloat(response.data.documents[0].address.x);
+          const lat = parseFloat(response.data.documents[0].address.y);
+          return { lat, lng };
+        } else {
+          return null;
+        }
+      } catch (error) {
+        console.error(error);
+        return null;
+      }
+    };
+    const { lat, lng } = await convertAddressToCoordinates();
+
+    console.log(lat, lng);
+
     const newAddress = data.address.split(" "); // 검색한 주소를 배열로 전환
     const dutyAddr1Depth = newAddress.splice(0, 2).join(" "); // 시,도 주소를 뽑아내기 위해서 인덱스 번호로 자름
     const dutyAddr2Depth = [...newAddress].join(" "); // 시,도 주소를 제외한 나머지 주소를 상세주소 변수에 추가
 
     setDutyAddr2Depth(dutyAddr2Depth); // 시,도 주소 변수 값으로 State변화
     setDutyAddr1Depth(dutyAddr1Depth); // 상세주소 변수 값으로 State변화
-    getAddrData(dutyAddr1Depth, dutyAddr2Depth); // 레지스터폼에서 props로 내려온 getAddrData 함수에 2가지 종류의 주소 데이터를 보냄
-    console.log(data);
+    getAddrData(dutyAddr1Depth, dutyAddr2Depth, lat, lng, fullAddress); // 레지스터폼에서 props로 내려온 getAddrData 함수에 2가지 종류의 주소 데이터를 보냄
+
     handleCloseModal(); // 주소 선택 시 모달 닫음
   };
 
