3df6b407c65a7da4f25491da30da6d397f7711b6
jim <jim77625516@gmail.com>
2023-06-11 12:42:13 +0900

    코드리뷰용

--- src/pages/info/ChildBox.jsx
+++ src/pages/info/ChildBox.jsx
@@ -4,9 +4,13 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 import styled from "styled-components";
-import colors from "../constants/colors";
+import colors from "../../constants/colors";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import axios from 'axios';
+
+const userToken =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Imp1bkBlbWFpbC5jb20iLCJzdWIiOjE4LCJpYXQiOjE2ODYzNzY1NDYsImV4cCI6MTcxNzkzNDE0Nn0.nDZfyySUgGh3_eHKfw4hoh8LYXsv2u5ljcB1NdyEGcM";
 
 // ChildBox의 저장버튼 구현
 const SaveButton = styled.button`
@@ -22,23 +26,42 @@ const SaveButton = styled.button`
   padding: 0% 2.5%;
 `;
 
-export const ChildBox = ({ id, onRemove, linkTo }) => {
-    const [selectedImage, setSelectedImage] = useState(null);
-    const [name, setName] = useState("");
-    const [birthYear, setBirthYear] = useState("");
-    const [birthMonth, setBirthMonth] = useState("");
-    const [birthDay, setBirthDay] = useState("");
+export const ChildBox = ({
+  id,
+  gender,
+  birth,
+  name,
+  memo,
+  image,
+  onRemove,
+  linkTo
+  }) => {
+    let modifyBirth = null
+    if(birth !== undefined || null){
+      modifyBirth = birth?.split('-')
+    } else {
+      modifyBirth=Array(null, null, null)
+    }
+    if(image?.length ===0){
+      image = null
+    }
+    const [selectedImage, setSelectedImage] = useState(image);
+    const [kidName, setKidName] = useState(name);
+    const [birthYear, setBirthYear] = useState(modifyBirth[0]);
+    const [birthMonth, setBirthMonth] = useState(modifyBirth[1]);
+    const [birthDay, setBirthDay] = useState(modifyBirth[2]);
     const [isEditable, setIsEditable] = useState(true);
 
     //사용자가 선택한 이미지 파일에 대한 참조(URL)를 selectedImage 상태에 저장
     const handleImageChange = (e) => {
       setSelectedImage(URL.createObjectURL(e.target.files[0]));
+      console.log(URL.createObjectURL(e.target.files[0]))
     };
 
     //사용자가 이름을 입력하면 그 값을 상태에 저장하는 역할
-    //이는 사용자의 입력을 실시간으로 'name' 상태에 반영하기 위한 것
-    const handleNameChange = (e) => {
-      setName(e.target.value);
+    //이는 사용자의 입력을 실시간으로 'kidname' 상태에 반영하기 위한 것
+    const handleKidNameChange = (e) => {
+      setKidName(e.target.value);
     };
 
     //사용자가 '생년'을 입력하면 그 값을 상태에 저장하는 역할
@@ -64,14 +87,14 @@ export const ChildBox = ({ id, onRemove, linkTo }) => {
 
     //버튼이 클릭되었을 때, 현재 입력 상태를 확정 편집을 불가능하게 만드는 역할
     //isEditable 상태를 false로 설정함으로써 이를 달성, 반대도 가능
-    const handleButtonClick = () => {
+    const handleButtonClick = async() => {
       // 이름, 생년, 월, 일 모두 채워져 있는지 확인
-      if (!name || !birthYear || !birthMonth || !birthDay) {
+      if (!kidName || !birthYear || !birthMonth || !birthDay) {
         alert("모든 칸을 채워주세요");
         return;
       }
       // 이름은 두 글자 이상이어야 함
-      if (name.length < 2) {
+      if (kidName.length < 2) {
         alert("이름은 최소 두 글자 이상이어야 합니다");
         return;
       }
@@ -86,6 +109,18 @@ export const ChildBox = ({ id, onRemove, linkTo }) => {
         return;
       }
       // 모든 유효성 검사를 통과하면 편집 가능 상태를 반전시킴
+      /*지금 이 상태는 에러가 날 수 밖에 없습니다. 왜냐하면 id를 어떻게 받을지에 대해서 우리는 생각을 해볼 필요가 있습니다.
+      만약. onRemove(id)에서 id를 제대로 받고 있다면 위에 'handleButtonClick'의 argument에서 id는 딱히 없어도 동작할
+      가능성은 있겠지만, 지금 현재로써는 저는 이게 동작하는지 가늠하기가 어렵군요*/
+      const originalBirth = `${birthYear}-${birthMonth}-${birthDay}`
+      await axios.patch(`http://34.64.69.226:3000/kid/${id}`, {
+        name: kidName,
+        birth: originalBirth
+      }, {
+        headers: {
+          Authorization: `Bearer ${userToken}`
+        }
+      });
       setIsEditable(!isEditable);
     };
 
@@ -140,17 +175,17 @@ export const ChildBox = ({ id, onRemove, linkTo }) => {
             <div>
               {isEditable ? (
                 <>
-                  <label htmlFor="nameInput" style={{ fontWeight: "bold" }}>이름 </label>
+                  <label htmlFor="kidNameInput" style={{ fontWeight: "bold" }}>이름 </label>
                   <InputBox
-                    id="nameInput"
+                    id="kidNameInput"
                     type="text"
-                    value={name}
-                    onChange={handleNameChange}
+                    value={kidName}
+                    onChange={handleKidNameChange}
                     style={{ fontWeight: "bold" }}
                   />
                 </>
               ) : (
-                <p style={{fontWeight: 'bold', textAlign: 'left'}}>{name}</p>
+                <p style={{fontWeight: 'bold', textAlign: 'left'}}>{kidName}</p>
               )}
             </div>
             <Space2 />
