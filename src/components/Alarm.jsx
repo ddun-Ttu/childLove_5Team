907178d8a11a9242428b7e33e30d74c283556545
2UJIN <83401978+2UJ1N@users.noreply.github.com>
2023-06-16 01:12:55 +0900

    design : alarm css 수정

--- src/components/Alarm.jsx
+++ src/components/Alarm.jsx
@@ -4,6 +4,7 @@ import { useQuery } from "react-query";
 import styled from "styled-components";
 import { calculateDday, formatDay, formatTime } from "../utils";
 import { Link } from "react-router-dom";
+import dayjs from "dayjs";
 
 export const Alarm = ({ onListChange }) => {
   // 현재 날짜
@@ -38,7 +39,9 @@ export const Alarm = ({ onListChange }) => {
                 : { backgroundColor: "rgba(0,173,92,0.5)" }
             }
           >
-            <h2>{calculateDday(activeDate, item.reservedDate)}</h2>
+            <ShowDday dday={calculateDday(activeDate, item.reservedDate)}>
+              <h2>{calculateDday(activeDate, item.reservedDate)}</h2>
+            </ShowDday>
 
             <ReservationList className="hosName">
               {item.hospital.dutyName}
@@ -59,7 +62,6 @@ export const Alarm = ({ onListChange }) => {
 const ReservationBox = styled(Link)`
   display: flex;
   justify-content: space-around;
-  text-align: left;
 
   width: 80%;
 
@@ -68,17 +70,31 @@ const ReservationBox = styled(Link)`
 
   border: 1px solid #d9d9d9;
   border-radius: 20px;
+`;
+
+const ShowDday = styled.div`
+  width: 20%;
+  height: auto;
+  border: ${({ dday }) => (dday === "Today" ? "1px solid #00ad5c" : "none")};
+  border-radius: 40px;
+  background-color: ${({ dday }) => (dday === "Today" ? "#00ad5c" : "white")};
+  color: ${({ dday }) => (dday === "Today" ? "white" : "black")};
+
+  padding-top: 10px;
+  padding-bottom: 10px;
 
   & > h2 {
     font-size: 20px;
-    font-weight: bold;
-    width: 20%;
+    font-weight: ${({ dday }) => (dday === "Today" ? "bold" : "normal")};
+
+    backgrond-color: white;
   }
 `;
 
 const ReservationList = styled.span`
   font-size: 18px;
   text-align: left;
+  padding-top: 10px;
 
   &.hosName {
     width: 40%;
@@ -94,6 +110,14 @@ const ReservationList = styled.span`
 
   &.resTime {
     width: 10%;
+
+    text-align: center;
+    align-items: center;
+
+    border: 1px solid #b2b2b2;
+    border-radius: 20px;
+
+    background-color: white;
   }
 `;
 
